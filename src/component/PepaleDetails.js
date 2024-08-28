1ff772930d73454efb61a839f78d4e824f74c312
Vijaykumar Patil <vijaykumarpatil960@gmail.com>
2024-08-28 14:46:11 +0530

    add done pelpledetail

--- src/component/PepaleDetails.js
+++ src/component/PepaleDetails.js
@@ -1,42 +1,43 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { peopleData } from "./Pepaledata";
 import { useParams } from "react-router-dom";
 
 function PepaleDetails() {
   const { id } = useParams();
-  const [pepaledetails, setpepaleDetail] = useState();
+  const [pepale, setPepale] = useState({});
+  useEffect(() => {
+    const data = peopleData.filter((ele) => ele.id === id);
+    setPepale(data[0]);
+  }, [id]);
 
-  const data = peopleData.filter((p) => p.id == id);
-  setpepaleDetail(data);
-
-  console.log(pepaledetails);
   return (
     <div>
       <div className="card" style={{ width: "18rem" }}>
-        {pepaledetails.map((ele) => {
-          return (
-            <>
-              <img className="card-img-top" src={ele.image} alt="img" />
-              <div className="card-body" key={ele.id}>
-                <h5 className="card-title">
-                  Name
-                  {ele.fName}
-                  {ele.lName}
-                </h5>
-                <p className="card-text">Email{ele.email}</p>
-              </div>
-              <ul className="list-group list-group-flush">
-                <li className="list-group-item">Role{ele.role}</li>
-                <li className="list-group-item">Number{ele.contactNumber}</li>
-                <li className="list-group-item">DOB{ele.DOB}</li>
-              </ul>
-              <div className="card-body">
-                <p className="card">Gender{ele.Gender}</p>
-                <p className="card">Teams{ele.teams}</p>
-              </div>
-            </>
-          );
-        })}
+        <>
+          <img
+            className="card-img-top"
+            src={pepale.image}
+            alt="img"
+            style={{ height: "180px" }}
+          />
+          <div className="card-body" key={pepale.id}>
+            <h5 className="card-title">
+              Name:-
+              {pepale.fName}
+              {pepale.lName}
+            </h5>
+            <p className="card-text">Email:-{pepale.email}</p>
+          </div>
+          <ul className="list-group list-group-flush">
+            <li className="list-group-item">Role:-{pepale.role}</li>
+            <li className="list-group-item">Number:-{pepale.contactNumber}</li>
+            <li className="list-group-item">DOB:-{pepale.DOB}</li>
+          </ul>
+          <div className="card-body">
+            <p className="card">Gender:-{pepale.Gender}</p>
+            <p className="card">Teams:-{pepale.teams}</p>
+          </div>
+        </>
       </div>
     </div>
   );
